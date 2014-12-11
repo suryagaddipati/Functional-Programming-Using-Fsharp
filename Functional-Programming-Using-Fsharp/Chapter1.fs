e1586e7e40c4d0737fafe73be86d7efa727e228b
Surya Gaddipati <surya.gaddipati@gmail.com>
2014-12-10 19:06:29 -0600

    More tests

--- Functional-Programming-Using-Fsharp/Chapter1.fs
+++ Functional-Programming-Using-Fsharp/Chapter1.fs
@@ -1,3 +1,10 @@
 ﻿namespace Functional_Programming_Using_Fsharp
 module Chapter1 =
  let g n = n + 4
+ let h x y = System.Math.Sqrt(x * x + y * y)
+ let g' = fun n -> n + 4
+ let h' = fun x y -> System.Math.Sqrt(x * x + y * y)
+
+ let rec f  = function
+           |0  -> 0
+           |n  -> n + f(n - 1)
--- Functional-Programming-Using-Fsharp/Chapter1Test.fs
+++ Functional-Programming-Using-Fsharp/Chapter1Test.fs
@@ -3,9 +3,19 @@
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open Functional_Programming_Using_Fsharp.Chapter1
 [<TestClass>]
-type One_1() = 
+type Chapter1() = 
     [<TestMethod>]
-    member this.``Adds 4``() =
+    member this.``1.1``() =
         Assert.AreEqual(8, g 4 )
+    [<TestMethod>]
+    member this.``1.2``() =
+        Assert.AreEqual(5.0, h 3.0 4.0 )
+    [<TestMethod>]
+    member this.``1.3``() =
+        Assert.AreEqual(8, g' 4 )
+        Assert.AreEqual(5.0, h' 3.0 4.0 )
+    [<TestMethod>]
+    member this.``1.4``() =
+        Assert.AreEqual(15, f 5 )
 
 
