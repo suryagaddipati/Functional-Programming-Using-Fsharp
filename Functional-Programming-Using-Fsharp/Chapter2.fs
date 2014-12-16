fa428e0dcaf159cf8d8d108627d26b47927278be
Surya Gaddipati <surya.gaddipati@gmail.com>
2014-12-15 19:36:09 -0600

    Start chapter 2

---
+++ Functional-Programming-Using-Fsharp/Chapter2.fs
@@ -0,0 +1,3 @@
+﻿namespace Functional_Programming_Using_Fsharp
+module Chapter2 =
+ let f n = (n%2 = 0 || n % 3 = 0) && n % 5 <> 0  
---
+++ Functional-Programming-Using-Fsharp/Chapter2Test.fs
@@ -0,0 +1,12 @@
+﻿namespace Functional_Programming_Using_Fsharp.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Functional_Programming_Using_Fsharp.Chapter2
+[<TestClass>]
+type Chapter2() = 
+    [<TestMethod>]
+    member this.``2.1``() =
+        Assert.IsTrue(f(24))
+        Assert.IsTrue(f(27))
+        Assert.IsFalse(f(29))
+        Assert.IsFalse(f(30))
